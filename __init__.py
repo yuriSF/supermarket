f83c5791d0e1dc617ce80a2ad574b2227f615ce2
yuriSF <yuri.yerastov@gmail.com>
2017-10-07 12:26:12 -0700

    inital

--- __init__.py
+++ __init__.py
@@ -15,29 +15,27 @@ def prepare_data(data):
         prepared_data.append(integer_line)
     return prepared_data
 
+def count_intersections(data):
+    intersections_count = []
+    for ind, row in enumerate(data):
+        current_row = data[ind]
+        reduced_data = data[ind+1:-1]
+        print len(reduced_data)
+        for row in reduced_data:
+            intersection = find_intersection(current_row, row)
+            if len(intersection) > 2:
+                intersections = [row[0] for row in intersections_count]
+                if intersection not in intersections:
+                    entry = [intersection, 1]
+                    intersections_count.append(entry)
+                else:
+                    for row in intersections_count:
+                        if intersection == row[0]:
+                            row[1] = row[1] + 1
+    return data
+
 data = prepare_data(data[0:1000])
-print data
-
-intersections_count = []
-for ind, row in enumerate(data):
-    current_row = data[ind]
-    reduced_data = data[ind+1:-1]
-    print len(reduced_data)
-    for row in reduced_data:
-        intersection = find_intersection(current_row, row)
-        if len(intersection) > 2:
-            intersections = [row[0] for row in intersections_count]
-            if intersection not in intersections:
-                entry = [intersection, 1]
-                intersections_count.append(entry)
-            else:
-                for row in intersections_count:
-                    if intersection == row[0]:
-                        row[1] = row[1] + 1
-                        print 'I am here'
-
-
-intersections_count.sort(key = lambda x: x[1], reverse = True)
-print intersections_count
-for item in intersections_count:
+all_intersections = count_intersections(data)
+all_intersections.sort(key = lambda x: x[1], reverse = True)
+for item in all_intersection:
     print item[0], item[1]
